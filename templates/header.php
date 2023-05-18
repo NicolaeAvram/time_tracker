47e73bd5aaa8a66fc9a3d161398789e24a911cfe
Nicu Avramiuc <nicuavramiuc@gmail.com>
2023-05-18 22:38:46 +0300

    elimina Departamente din navbar utilizator

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,9 @@
 <?php if(isset($_SESSION['username']) && !empty($_SESSION['username'])):?>
     <h3>Bine ati venit, <?php echo $_SESSION['prenume']?></h3>
     <a href="index.php">Acasa</a><br>
+<?php if($_SESSION['rol'] =='admin'):?>
     <a href="departamente.php">Departamente</a><br>
+<?php endif ?>    
     <a href="register.php">Register</a><br>
     <a href="logout.php">Logout</a><br>
 <?php else:?>
